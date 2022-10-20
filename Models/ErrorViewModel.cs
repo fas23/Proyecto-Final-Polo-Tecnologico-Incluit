15baed2f6151c26fcb9594a0279ed0a2333b1a9b
Fabián Abel Sajama <sajama230@gmail.com>
2022-10-20 08:50:17 -0300

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Proyecto_Final_Polo_Tecnologico_Incluit.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
